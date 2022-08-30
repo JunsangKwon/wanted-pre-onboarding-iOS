f0241622a8b0b05b886bb991cd0737399de88932
권준상 <wnstkdsun97@naver.com>
2022-08-30 14:21:35 +0900

    first commit

---
+++ wanted-pre-onboarding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wanted-pre-onboarding
+//
+//  Created by 권준상 on 2022/08/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
